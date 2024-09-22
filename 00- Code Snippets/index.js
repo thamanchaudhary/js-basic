03123e80ba2fb227948ac685a3c302d35a23708f
Thaman Tharu (TRC) <thamanchaudhary65@gmail.com>
2024-09-22 17:43:23 +0545

    datatypes added

---
+++ 00- Code Snippets/index.js
@@ -0,0 +1,18 @@
+// User proper variable names and camelCase
+
+const firstName = "Thaman";
+const lastName = "Tharu";
+
+const videos = assets.filter((asset) => asset.type === "video");
+/*
+    1. Use proper variable names and camelCase
+    2. Use arrow function syntax
+    3. Use filter method to filter out the videos
+*/
+
+// Be careful with comparison using the loose equality operator
+[100] == 100; // true
+// [100] === 100; // false  //
+
+"100" == 100; // true
+"100" === 100; // false
---
+++ 3- Mastering Javascript datatypes/datatypes.js
@@ -0,0 +1,57 @@
+/* JavaScript Data Types
+    - string
+    - number
+    - boolean
+    - object
+    - function
+    - undefined
+    - null
+    - symbol
+*/
+
+// String
+const stringExample = "Thaman Ram Tharu";
+ stExample = typeof stringExample;
+ console.log(stExample); // string
+
+// Number
+const numberExample = 123;
+ numExample = typeof numberExample;
+ console.log(numExample); // number
+
+// Boolean
+const booleanExample = true;
+ boolExample = typeof booleanExample;
+ console.log(boolExample); // boolean
+
+// Object
+const objectExample = {name: "Thaman", age: 25};
+ objExample = typeof objectExample;
+ console.log(objExample); // object
+
+// Function
+const functionExample = function() {
+    return "Hello Thaman";
+}
+ funcExample = typeof functionExample;
+ console.log(funcExample); // function
+
+// Undefined
+let undefinedExample;
+ undExample = typeof undefinedExample;
+ console.log(undExample); // undefined
+
+
+// Null
+const nullExample = null;
+ nulExample = typeof nullExample;
+ console.log(nulExample); // object
+
+// Symbol
+const symbolExample = Symbol();
+ symExample = typeof symbolExample;
+ console.log(symExample); // symbol
+
+ 
+
+
